260b80297aa72322048121ccd49823b398fd0027
Maher <mohamedmaher055@gmail.com>
2021-04-01 15:34:12 +0200

    init chat

---
+++ app/Http/Controllers/Front/Chat/MessageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Front\Chat;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MessageController extends Controller
+{
+    public function conversation($vendor_id) {
+
+    }
+}
