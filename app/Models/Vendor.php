5b110330e5a8a5e08d368a2b349a4b24c03a65f5
Maher <mohamedmaher055@gmail.com>
2021-08-17 11:36:37 +0200

    fix

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -22,7 +22,7 @@ class Vendor extends Authenticatable implements HasMedia, MustVerifyEmail
 {
     use SoftDeletes, HasFactory, Notifiable, InteractsWithMedia, DefaultImage, HasTranslations, CascadeSoftDeletes, BelongsToThrough;
 
-    protected $fillable = ['name', 'country_id', 'subCategory_id', 'email' , 'contacts', 'latitude', 'longitude', 'shipping_prices',
+    protected $fillable = ['name', 'country_id', 'subCategory_id', 'email' , 'contacts', 'latitude', 'longitude',
         'membership_id', 'password', 'facebook_url', 'twitter_url' ,'pinterest_url', 'instagram_url', 'youtube_url', 'website_url', 'slug'];
 
     protected $guard = 'vendor';
@@ -40,8 +40,7 @@ class Vendor extends Authenticatable implements HasMedia, MustVerifyEmail
 
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'contacts'          => 'array',
-        'shipping_prices'   => 'array',
+        'contacts'          => 'array'
     ];
 
     public function registerAllMediaConversions(): void {
