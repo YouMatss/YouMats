fdfcbf5e7fc0170e2a26df8974c1be58bbd1359a
Mohamed Maher <mohamedmaher055@gmail.com>
2022-09-21 23:56:12 +0200

    Update DeliveryFilter.php

--- app/Helpers/Classes/DeliveryFilter.php
+++ app/Helpers/Classes/DeliveryFilter.php
@@ -9,9 +9,9 @@ class DeliveryFilter implements Filter
 {
     public function __invoke(Builder $query, $value, string $property)
     {
-            $query->where('price', '>', 0)
-                ->whereNotNull('shipping_id')
-                ->where('specific_shipping', true)
-                ->get();
+            $query->where(function ($q) {
+                    $q->whereNotNull('shipping_id')
+                        ->orWhere('specific_shipping', true);
+                })->get();
     }
 }
