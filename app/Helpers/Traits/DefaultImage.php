2f8e18c43686ca39ada62dd2324cafe7a938fcf8
Mohamed Maher <mohamedmaher055@gmail.com>
2022-12-08 00:59:45 +0200

    hot fixes

--- app/Helpers/Traits/DefaultImage.php
+++ app/Helpers/Traits/DefaultImage.php
@@ -6,7 +6,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 trait DefaultImage {
 
-    protected static string $defaultImage = 'https://via.placeholder.com/150/003f91/f5f5f5.webp?text=YouMats';
+    protected static string $defaultImage = 'https://youmats-media.s3.me-central-1.amazonaws.com/extra/placeholder.webp';
     protected static string $defaultCover = 'https://via.placeholder.com/1350x300/f5f5f5/003f91.webp?text=YouMats';
 
     public function getFirstMediaUrlOrDefault(string $collectionName = '', string $conversionName = 'cropper')
--- resources/views/front/category/index.blade.php
+++ resources/views/front/category/index.blade.php
@@ -105,11 +105,13 @@
         </div>
     </div>
     <div class="mb-6 bg-gray-7 py-6">
+        @if($category->getTranslation('title', app()->getLocale(), false))
         <div class="container mb-8">
             <div class="d-flex justify-content-between border-bottom border-color-1 flex-lg-nowrap flex-wrap border-md-down-top-0 border-md-down-bottom-0 mb-3 rtl">
-                <h1 class="section-title section-title__full mb-0 pb-2 font-size-22">{{(!empty($category->title)) ? $category->title : $category->name}}</h1>
+                <h1 class="section-title section-title__full mb-0 pb-2 font-size-22">{{$category->getTranslation('title', app()->getLocale(), false)}}</h1>
             </div>
         </div>
+        @endif
         <div class="container">
             <div class="row flex-nowrap flex-md-wrap overflow-auto overflow-md-visble rtl">
                 @foreach($children as $child)
