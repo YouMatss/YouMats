52fdd185456d087ab73776ce7647024e1327b650
Mohamed Maher <mohamedmaher055@gmail.com>
2022-12-16 18:18:53 +0200

    edits
    - 404 page for every page except products
    - Speed (compress js and css)
    - Redirect 301 to home for 404 page (products)
    - 404 page

--- resources/views/front/layouts/partials/assets_js.blade.php
+++ resources/views/front/layouts/partials/assets_js.blade.php
@@ -1,44 +1,3 @@
-{{--<!-- JS Global Compulsory -->--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/jquery/dist/jquery.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/jquery-migrate/dist/jquery-migrate.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/popper.js/dist/umd/popper.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/bootstrap/bootstrap.min.js"></script>--}}
-
-{{--<!-- JS Implementing Plugins -->--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/appear.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/jquery.countdown.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/hs-megamenu/src/hs.megamenu.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/svg-injector/dist/svg-injector.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/jquery-validation/dist/jquery.validate.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/fancybox/jquery.fancybox.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/ion-rangeslider/js/ion.rangeSlider.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/typed.js/lib/typed.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/slick-carousel/slick/slick.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/appear.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/intlTelInput.min.js"></script>--}}
-
-{{--<!-- JS Electro -->--}}
-{{--<script defer src="{{front_url()}}/assets/js/hs.core.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.countdown.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.header.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.hamburgers.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.unfold.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.focus-state.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.malihu-scrollbar.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.validation.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.fancybox.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.onscroll-animation.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.slick-carousel.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.quantity-counter.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.range-slider.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.show-animation.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.svg-injector.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.scroll-nav.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.go-to.js"></script>--}}
-{{--<script defer src="{{front_url()}}/assets/js/components/hs.selectpicker.js"></script>--}}
-
 <script src="{{mix('/assets/js/app.min.js')}}"></script>
 
 <!-- moment -->
@@ -54,8 +13,6 @@
         }
     });
 </script>
-<!-- Socket.io -->
-<script defer src="https://cdn.socket.io/4.0.0/socket.io.min.js" integrity="sha384-DkkWv9oJFWLIydBXXjkBWnG1/fuVhw8YPBq37uvvD6WSYRFRqr21eY5Dg9ZhmWdy" crossorigin="anonymous"></script>
 
 {{--@if(is_company())--}}
 {{--<script defer>--}}
