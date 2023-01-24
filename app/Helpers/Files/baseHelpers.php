80e710c21634dbd9dedad9e60e84afa29f7f3ff4
Mohamed Maher <mohamedmaher055@gmail.com>
2023-01-24 20:15:15 +0200

    change message

--- app/Helpers/Files/baseHelpers.php
+++ app/Helpers/Files/baseHelpers.php
@@ -72,11 +72,7 @@ if (!function_exists('cartOrChat')) {
         $viewDetails = '<a class="cart-chat-category btn-primary transition-3d-hover"
                             href="'.route('front.category', [generatedNestedSlug($product->category->ancestors()->pluck('slug')->toArray(), $product->category->slug)]).'">'.__('product.category_href').'</a>';
 
-//        href="https://wa.me/+966'.$product->phone().'"
-        $integration_number = nova_get_setting('whatsapp_integration');
-        if($product->vendor->manage_by_admin)
-            $integration_number = nova_get_setting('whatsapp_manage_by_admin');
-        $chat = '<div><a target="_blank" href="https://wa.me/'. $integration_number .'?text='.$product->whatsapp_message().'"
+        $chat = '<div><a target="_blank" href="'.$product->whatsapp_message().'"
                     class="cart-chat-category btn btn-primary transition-3d-hover">
                         <i class="fa fa-comments"></i> &nbsp;' . __("general.chat_button") . '
                     </a>
