6b2bb337c7430adb747b0304a23d69d73146ff84
Zakaria Tlilani <zakaria@youmats.com>
2023-06-11 07:04:39 +0100

    welcome popup edits

--- resources/views/front/layouts/partials/welcome-popup.blade.php
+++ resources/views/front/layouts/partials/welcome-popup.blade.php
@@ -1,35 +1,22 @@
-<div class="modal fade" id="myModal">
-    <div class="modal-dialog" role="document" style="max-width: 600px">
-        <div class="modal-content st_model_new">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">{{__('general.welcome_message')}}</h5>
-            </div>
-            <div class="modal-body">
+@if(!\Illuminate\Support\Facades\Session::get('userType') && !is_company() && !is_individual())
+        <div class="container modal-content st_model_new" id="myModal" style="min-width: 100%;margin: 0;bottom: 0;position: fixed;z-index: 10;">
+            <div class="container modal-body" style="padding: 0.5rem;">
                 <div class="row">
-                    <div class="col-md-6">
-                        <a class="select_reg typeIntroduceButton" data-url="{{route('front.introduce', ['individual'])}}">
-                            {{__('general.continue_as_individual', [], 'ar')}} <br/>
-                            {{__('general.continue_as_individual', [], 'en')}}
+                    <div class="col-md-6" style="padding-bottom: 0.25rem!important;">
+                        <h3 class="modal-title" style="text-align: center !important;" id="exampleModalLabel">{{__('general.welcome_message')}}</h3>
+                    </div>
+                    <div class="col-md-3" style="padding-bottom: 0.25rem!important;">
+                        <a class="select_reg typeIntroduceButton" style="margin: 0;" data-url="{{route('front.introduce', ['individual'])}}">
+                            {{__('general.continue_as_individual')}}
                         </a>
                     </div>
-                    <div class="col-md-6">
-                        <a class="select_reg typeIntroduceButton" data-url="{{route('front.introduce', ['company'])}}">
-                            {{__('general.continue_as_company', [], 'ar')}} <br/>
-                            {{__('general.continue_as_company', [], 'en')}}
+                    <div class="col-md-3">
+                        <a class="select_reg typeIntroduceButton" style="margin: 0;" data-url="{{route('front.introduce', ['company'])}}">
+                            {{__('general.continue_as_company')}}
                         </a>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-
-@if(!\Illuminate\Support\Facades\Session::get('userType'))
-<script type="text/javascript">
-    document.addEventListener('DOMContentLoaded', function() {
-        $(document).ready(function() {
-            $('#myModal').modal({backdrop: 'static', keyboard: false});
-        });
-    });
-</script>
 @endif
+
